15948465f436d6258f5ce23e4615ed51debf4aa6
Jacob Greenwald <jeg4t@virginia.edu>
2019-05-05 17:21:51 -0400

    enqueuing js

--- FormFilter.php
+++ FormFilter.php
@@ -6,10 +6,12 @@ include 'FormFilterSettings.php';
 
 class FormFilter {
 	public function __construct() {
-		add_action( 'the_content', array($this, 'say_hello' ) );
+		add_action( 'wp_enqueue_scripts', array($this, 'localize_variables' ) );
 	}
-	public function say_hello() {
-		echo '<h1>Hello!!!</h1>';
+	public function localize_variables() {
+		$admin_url = esc_url( admin_url('admin-post.php') );
+		wp_enqueue_script( 'form_filter_scripts', plugin_dir_url( __FILE__ ) . 'js/formFilter.js' );
+
 	}
 }
 
