1a374d24effdc204184352b6068a7b95cde5d697
Jacob Greenwald <jeg4t@virginia.edu>
2019-05-05 18:16:49 -0400

    formatting db options and passing to front end

--- FormFilter.php
+++ FormFilter.php
@@ -8,10 +8,24 @@ class FormFilter {
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', array($this, 'localize_variables' ) );
 	}
-	public function localize_variables() {
-		$admin_url = esc_url( admin_url('admin-post.php') );
-		wp_enqueue_script( 'form_filter_scripts', plugin_dir_url( __FILE__ ) . 'js/formFilter.js' );
 
+
+	public function localize_variables() {
+		$admin_post_url = esc_url( admin_url('admin-post.php') );
+
+		// pull list of form ids from database
+		// remove any special characters and split into array
+		$formIds = get_option( 'list_of_form_ids' );
+		$formIds = preg_replace( '/\s+/', '', $formIds );
+		$formIds = explode( ",", $formIds );
+
+		wp_enqueue_script( 'form_filter_scripts', plugin_dir_url( __FILE__ ) . 'js/formFilter.js', false, '', true );
+		
+		// localize variables 
+		wp_localize_script('form_filter_scripts', 'formFilter', array(
+    	'adminPost' => $admin_post_url,
+			'formIds' => $formIds
+		));
 	}
 }
 
